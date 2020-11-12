e3cd21b5a79b2ef57f5d1ec113b9a0df25d8b970
Andy Pilyugin <reaferon@yandex.ru>
2020-11-12 16:44:19 +0300

    JS

---
+++ helpers/js/function__overlay.js
@@ -0,0 +1,8 @@
+// function overlay() {
+//   let overlay = document.querySelector('.overlay');
+//   if(overlay.classList.contains('overlay_active')) {
+//     overlay.classList.remove( 'overlay_active');
+//   } else {
+//     overlay.classList.add( 'overlay_active');
+//   }
+// }
