19a2349c5222aa6bb486d81289d2f1608e6c27fa
Andy Pilyugin <reaferon@yandex.ru>
2020-11-16 00:30:07 +0300

    Список продуктов

--- app.min.js
+++ app.min.js
@@ -1,8 +1,11 @@
 document.querySelector('.header__search').onclick = function (e) {
-  console.log('sdf');
   let header__search_fieldset = document.querySelector('.header__search-fieldset');
 
-  if (!e.target.matches('.header__search .header__search-fieldset') && getComputedStyle(header__search_fieldset)['position'] == 'fixed') {
+  if (
+    !e.target.matches('.header__search .header__search-fieldset') && 
+    !e.target.matches('.header__search .header__search-input') && 
+    getComputedStyle(header__search_fieldset)['position'] == 'fixed'
+    ) {
     overlay();
     document.querySelector('.header__search-fieldset').classList.add('header__search-fieldset_active');
     document.querySelector('.header__search-input').focus();
